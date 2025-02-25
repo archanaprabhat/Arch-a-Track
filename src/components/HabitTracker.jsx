dcf124b35d9a28c387f7d309cf79a66016357379
archanaprabhat <archanaprabhath2002@gmail.com>
2025-02-25 11:01:23 +0530

    add delete functionality

--- src/components/HabitTracker.jsx
+++ src/components/HabitTracker.jsx
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-import { ChevronLeft, ChevronRight, Plus } from "lucide-react";
+import { ChevronLeft, ChevronRight, Plus, Check , X, Trash2 } from "lucide-react";
 export default function HabitTracker() {
    const [currentDate, setCurrentDate] = useState(new Date());
    const[newHabit, setNewHabit] = useState("");
@@ -50,23 +50,33 @@ export default function HabitTracker() {
           <input type="text" placeholder='Add a new habit...' value={newHabit} onChange={(e) => setNewHabit(e.target.value)} className='p-6 rounded-lg border text-2xl flex-1 focus:outline-none shadow-lg focus:border-pink-400 transition ' />
           <button onClick={addHabit}><Plus size={60} color="#590d22" strokeWidth={3} /></button>
         </div>
-        <div className='w-full'>
-          <table >
+        <div className='overflow-x-auto'>
+          <table  >
             <thead>
               <tr>
-                <td className="border p-4 text font-bold">Habits</td>
+                <th className="border  p-4 text-center font-bold text-lg ">Habits</th>
               {Array.from({length:31}, (_, i) =>(
-                 <th key={i} className="border p-3"> {i+1} </th>
+                 <th key={i} className="border p-3 w-16 text-lg "> {i+1} </th>
               ))}
+              <th className="border p-4 w-24 font-bold text-lg">Delete</th>
                </tr>  
              </thead>
-             <tbody>
+             <tbody>  
               {habits.map((habit, habitIndex) => (
                 <tr key={habit.id}>
-                  <td className="border p-2 font-bold">{habit.name}</td>
+                  <td className="border p-2 font-medium text-center text-lg">{habit.name}</td>
                   {habit.checked.map((checked, dayIndex) => (
-                    <td key={dayIndex} onClick={() => toggleDay(habitIndex,dayIndex)} className='border p-2 text-center cursor-pointer'>{checked ? "✔️" : "❌"}</td>
+                    <td key={dayIndex} onClick={() => toggleDay(habitIndex,dayIndex)} className='border p-2 text-center cursor-pointer hover:bg-red-200 transition-colors'>{checked ? (
+                      <Check className="text-green-800 mx-auto" size={28}  strokeWidth={4} />
+                    ) : (
+                      <X className="text-red-400 mx-auto" size={28} />
+                    )}</td>
                   ))}
+                  <td className='p-2 border text-center'>
+                  <button onClick={() => {setHabits(habits.filter(h=> h.id !== habit.id))}}>
+                  <Trash2 size={28} />
+                  </button>
+                  </td>
                 </tr>
               ))}
              </tbody>
