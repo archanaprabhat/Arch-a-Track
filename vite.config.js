e9203df4374e1cd9c07fa27e2ee556e2101b86a9
archana_prabhat <archanaprabhath2002@gmail.com>
2025-06-19 12:05:42 +0530

    Vite config change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/Arch-a-Track/",
-  
+  base: process.env.NODE_ENV === 'production' ? '/Arch-a-Track/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
